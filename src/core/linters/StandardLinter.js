40e7e86bc59cd85a89201cc80b54ef0d3ae5d8ae
Emil Persson <emil.persson@creuna.se>
2017-02-01 15:57:55 +0100

    Add support for standardts linter with TypeScript

--- src/core/linters/StandardLinter.js
+++ src/core/linters/StandardLinter.js
@@ -1,22 +1,8 @@
-export default class StandardLinter {
-  id = 'standard'
-  name = 'Standard Style'
+class StandardJS {
   jsxHeader = '/** @jsx Node */'
+  packageName = 'standard'
 
-  constructor (logger, fs, path) {
-    this._logger = logger
-    this._fs = fs
-    this._path = path
-  }
-
-  async install (directory, packageManager, taskRunner, compiler, testRunner) {
-    this._logger.fine('Installing Standard Style linter')
-
-    packageManager.install('standard', { dev: true })
-
-    const packageJson = this._path.resolve(directory, 'package.json')
-    const pkg = await this._fs.readJson(packageJson)
-
+  modifyPackageJson (pkg, compiler, testRunner) {
     pkg.standard = {}
 
     if (compiler && compiler.id === 'babel') {
@@ -28,19 +14,50 @@ export default class StandardLinter {
     if (testRunner && testRunner.globals) {
       pkg.standard.globals = testRunner.globals
     }
+  }
+}
 
-    if (Object.keys(pkg.standard).length > 0) {
+class StandardTS {
+  jsxHeader = null
+  packageName = 'standardts'
+
+  modifyPackageJson () {}
+}
+
+export default class StandardLinter {
+  id = 'standard'
+  name = 'Standard Style'
+
+  constructor (logger, fs, path) {
+    this._logger = logger
+    this._fs = fs
+    this._path = path
+  }
+
+  async install (directory, packageManager, taskRunner, compiler, testRunner) {
+    this._logger.fine('Installing Standard Style linter')
+
+    const implementation = compiler && compiler.id === 'typescript'
+      ? new StandardTS()
+      : new StandardJS()
+
+    this.jsxHeader = implementation.jsxHeader
+
+    packageManager.install(implementation.packageName, { dev: true })
+
+    const packageJson = this._path.resolve(directory, 'package.json')
+    const pkg = await this._fs.readJson(packageJson)
+
+    implementation.modifyPackageJson(pkg, compiler, testRunner)
+
+    if (pkg.standard && Object.keys(pkg.standard).length > 0) {
       await this._fs.writeJson(packageJson, pkg)
     }
 
-    if (taskRunner == null) {
-      return
-    }
-
-    if (taskRunner.commands.test != null) {
-      taskRunner.add('test', 'standard && ' + taskRunner.commands.test)
+    if (taskRunner && taskRunner.commands.test != null) {
+      taskRunner.add('test', implementation.packageName + ' && ' + taskRunner.commands.test)
     } else {
-      taskRunner.add('test', 'standard')
+      taskRunner.add('test', implementation.packageName)
     }
   }
 }
